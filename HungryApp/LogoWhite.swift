d34bacc946cd7b86f1aa14d89f071e9671542f87
nasrinlida <nasrinakterlida@gmail.com>
2021-05-04 05:30:31 +0600

    LogoWhite screen is completed.

---
+++ HungryApp/LogoWhite.swift
@@ -0,0 +1,22 @@
+//
+//  LogoWhite.swift
+//  HungryApp
+//
+//  Created by nasrinlida on 4/5/21.
+//
+
+import SwiftUI
+
+struct LogoWhite: View {
+    var body: some View {
+        ZStack {
+            Image("HungryLogoWhite")
+        }
+    }
+}
+
+struct LogoWhite_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoWhite()
+    }
+}
