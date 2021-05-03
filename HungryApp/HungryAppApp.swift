624f28470dfcd5a7b8bbfecbd69b189c8247d396
nasrinlida <nasrinakterlida@gmail.com>
2021-05-04 04:44:38 +0600

    This is the initial commit for HungryApp

---
+++ HungryApp/HungryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungryAppApp.swift
+//  HungryApp
+//
+//  Created by nasrinlida on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HungryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
